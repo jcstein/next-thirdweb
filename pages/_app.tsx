6617551e0172a2a7dd03c3f75bca25bbc801b7f3
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-08-03 01:15:35 -0400

    🛸

---
+++ pages/_app.tsx
@@ -0,0 +1,96 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+  useSigner,
+} from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import {
+  connectorsForWallets,
+  wallet,
+  RainbowKitProvider,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { ThirdwebSDKProvider } from "@thirdweb-dev/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+
+const { chains, provider } = configureChains(
+  [chain.rinkeby],
+  [
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/eth_rinkeby",
+        };
+      },
+    }),
+    publicProvider(),
+  ]
+);
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      wallet.metaMask({ chains, shimDisconnect: true }),
+      wallet.walletConnect({ chains }),
+      wallet.coinbase({ appName: "Ankr Multichain NFT Gallery", chains }),
+      wallet.rainbow({ chains }),
+    ],
+  },
+  {
+    groupName: "Others",
+    wallets: [
+      wallet.argent({ chains }),
+      wallet.brave({
+        chains,
+        shimDisconnect: true,
+      }),
+      wallet.imToken({ chains }),
+      wallet.injected({
+        chains,
+        shimDisconnect: true,
+      }),
+      wallet.ledger({
+        chains,
+      }),
+      wallet.steak({ chains }),
+      wallet.trust({ chains, shimDisconnect: true }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
+
+const { data: signer } = useSigner();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={midnightTheme()} coolMode>
+          <ThirdwebSDKProvider
+            signer={signer}
+            provider={wagmiClient.provider}
+            queryClient={wagmiClient.queryClient}
+          >
+            <ColorModeScript />
+            <Component {...pageProps} />
+          </ThirdwebSDKProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
